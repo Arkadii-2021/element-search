1e053eeeceb56d29b9cde9f0fbef54d1345a7ef9
Arkadii Kozhuhow <arkadii_kozhuhow@mail.ru>
2022-07-03 14:44:10 +0700

    add element-search -hw

---
+++ element-search/menu/task.js
@@ -0,0 +1,6 @@
+const menuActive = document.getElementsByClassName('menu_sub');
+const mainMenu = document.getElementsByClassName("menu menu_main");
+
+for (menuItems of mainMenu) {
+	console.log(menuItems);
+}
